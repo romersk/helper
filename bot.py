ff987d767fcc00d8f7e92068ee62d575eff0b27c
romersk <56487731+romersk@users.noreply.github.com>
2025-04-16 21:50:20 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 import logging
 from datetime import datetime
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
-
+from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackContext
+from telegram.ext import filters
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
@@ -88,7 +88,7 @@ def main():
     dp = updater.dispatcher
 
     dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(MessageHandler(Filters.text & ~Filters.command, process_data))
+    dp.add_handler(MessageHandler(filters.TEXT & ~Filters.command, process_data))
     dp.add_error_handler(error)
 
     updater.start_polling()
